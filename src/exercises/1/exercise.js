fcd96e35540561d99fbd127fe55f13c0992c04aa
Tarak <tarakaswathi2001@gmail.com>
2023-01-20 17:33:06 +0530

    1st exercise done

--- src/exercises/1/exercise.js
+++ src/exercises/1/exercise.js
@@ -3,7 +3,9 @@
 //  - Fix the issue.
 
 function printOneToTen() {
-  for (const number = 1; number <= 10; number++) {
+  for (var number = 1; number <= 10; number++) {
     console.log(`\n${number}`);
   }
 }
+//reason for failure is: assignment to constant variable 
+//It's fixed by changing const to var
