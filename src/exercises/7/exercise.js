65bb749aef2f747c811d25704775caf8b021fd83
Tarak <tarakaswathi2001@gmail.com>
2023-01-23 11:18:24 +0530

    7th Exercise done

--- src/exercises/7/exercise.js
+++ src/exercises/7/exercise.js
@@ -4,4 +4,7 @@
 function commaSeparatedString(inputArray) {
   // Given an Array like ["firstName","lastName"]
   // Return a comma separated String like "firstName,lastName"
+  let finalString=inputArray.join(',');
+  console.log(finalString);
 }
+//used join method with a seperator of ,
