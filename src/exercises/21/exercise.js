7388b5cd645192cceb4b5ae90d7de3df03fe15f2
Tarak <tarakaswathi2001@gmail.com>
2023-01-23 17:01:56 +0530

    21st exercise done

--- src/exercises/21/exercise.js
+++ src/exercises/21/exercise.js
@@ -1,1 +1,12 @@
 // What are the different levels of logs and how do we print them to the console?
+/* Different levels of Logs:
+ TRACE
+ DEBUG
+ INFO
+ WARN
+ ERROR
+ FATAL
+ */
+
+/*console.log(),console.info(),console.warn(),console.error(),console.trace(),console.debug(),console.fatal() can be used to print these levels on to the console */
+ 
