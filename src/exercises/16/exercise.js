b7da0522b507393afa86425f4dece061c37425e3
Tarak <tarakaswathi2001@gmail.com>
2023-01-23 16:02:24 +0530

    16th exercise done

--- src/exercises/16/exercise.js
+++ src/exercises/16/exercise.js
@@ -4,4 +4,22 @@
 
 class Leave {
   //...
+   
+  constructor(employeeId,employeeName,noOfDays,salary,leavesCount){
+    this.employeeId=employeeId;
+    this.employeeName=employeeName;
+    this.noOfDays=noOfDays;
+    this.salary=salary;
+    this.leavesCount=leavesCount;
+  }
+  finalPay(salary,days){
+    return salary-(days*500);
+  }
+  remainingLeaves(leavesCount,noOfDays)
+  {
+    return leavesCount-noOfDays;
+  }
 }
+//class variables:employeeId,employeeName,noOfDays,salary,leavesCount
+//let leave1=new Leave('101','Swathi',4,50000,16);
+//console.log(leave1.remainingLeaves(leave1.leavesCount,leave1.noOfDays));
