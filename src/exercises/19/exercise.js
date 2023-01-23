0e3bca568738e1f3b48f8177b245528369e311e7
Tarak <tarakaswathi2001@gmail.com>
2023-01-23 16:42:27 +0530

    exercise.js

--- src/exercises/19/exercise.js
+++ src/exercises/19/exercise.js
@@ -1,3 +1,9 @@
 // Comment below what did you understand from the previous 3 exercises about
 // - prototype
 // - __proto__
+
+//Protype: it holds shared attributes and behaviors of instances.Used to inherit properties from javascript as it is 
+//available in every function declaration.
+
+//Proto:It is an actual object that provides a way to inherit properties from JavaScript with the help of an object which is created with new. 
+//Every object with behavior associated has internal property [[prototype]].
