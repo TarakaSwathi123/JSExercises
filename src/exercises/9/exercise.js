f336b1591cd0316cfe9a587036f8b78f8fcf202a
Tarak <tarakaswathi2001@gmail.com>
2023-01-23 12:22:02 +0530

    9th Exercise done

--- src/exercises/9/exercise.js
+++ src/exercises/9/exercise.js
@@ -1,7 +1,14 @@
 // Complete the below function.
 // - Use Array methods.
 // - Do not use loops
+function Multiply(element){
+  return element*2;
+}
 
 function multiplyArrayByTwo(inputArray) {
+  inputArray=inputArray.map(Multiply)
   console.log(`Given input array is ${inputArray}`);
 }
+
+
+
