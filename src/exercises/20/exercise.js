3648a9802a8b2919a516d8e17584aabe7f908d46
Tarak <tarakaswathi2001@gmail.com>
2023-01-23 16:47:44 +0530

    exercise.js

--- src/exercises/20/exercise.js
+++ src/exercises/20/exercise.js
@@ -1,3 +1,8 @@
 // What is debugger;
 // - Comment your understanding of it.
 // - Paste the link of the MDN article here.
+/*The debugger statement invokes any available debugging functionality, such as setting a breakpoint. 
+If no debugging functionality is available, this statement has no effect.
+When the debugger is invoked, execution is paused at the debugger statement. It is like a breakpoint in the script source.*/
+
+//MDN link:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/debugger
