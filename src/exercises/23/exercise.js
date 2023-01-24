942a221ed184f70baa5cde6fdf3b82a0444049ee
Tarak <tarakaswathi2001@gmail.com>
2023-01-24 15:16:59 +0530

    23rd exercise done

--- src/exercises/23/exercise.js
+++ src/exercises/23/exercise.js
@@ -1,6 +1,11 @@
-// What does a call to the noDelayPrint() do.
+// What does a call to the noDelayPrint() do./
+/*It printed You asked me to print the sheet 0
+undefined
+Printing sheet number undefined */
 // Is 'this' inside print function referrring to an instance of Printer
-// Is the correct sheet printed?
+/* No */
+
+// Is the correct sheet printed? No
 //  Comment your understanding
 //  Provide a fix.
 
@@ -15,13 +20,17 @@ class Printer {
 
   noDelayPrint() {
     console.log(`You asked me to print the sheet ${this.index}`);
-    setTimeout(this.print, 0); // 0 milli-sec delay
+    setTimeout(() => {
+      this.print();
+    } ,0);// 0 milli-sec delay
     this.index++;
   }
 
-  noDelayPrint() {
-    console.log(`You asked me to print the sheet ${this.index}`);
-    setTimeout(this.print, 1000); //1 sec dely
-    this.index++;
-  }
+  //noDelayPrint() {
+    //console.log(`You asked me to print the sheet ${this.index}`);
+   // setTimeout(this.print, 1000); //1 sec dely
+    //this.index++;
+  //}
 }
+var printer1= new Printer();
+printer1.noDelayPrint();
