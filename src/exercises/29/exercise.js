46ce7bfdce14720d1bb121db01bbc11365794624
Tarak <tarakaswathi2001@gmail.com>
2023-01-24 16:07:46 +0530

    29th exercise done

--- src/exercises/29/exercise.js
+++ src/exercises/29/exercise.js
@@ -1,6 +1,17 @@
 // - Explain currying.
+//It is a technique in functional programming, transformation of the function of multiple arguments 
+//into several functions of a single argument in sequence. 
+
 // - Convert the below function to curry function
 
-function calculateVolume(length, breadth, height) {
-  return length * breadth * height;
+//function calculateVolume(length, breadth, height) {
+  //return length * breadth * height;
+//}
+function calculateVolume(length) {
+  return function (breadth) {
+      return function (height) {
+          return length * breadth * height;
+      }
+  }
 }
+
