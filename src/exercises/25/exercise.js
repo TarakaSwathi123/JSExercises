0469d1544052d993016c79e15e1a85ecbc4c3e62
Tarak <tarakaswathi2001@gmail.com>
2023-01-24 13:52:42 +0530

    25th exercise done

--- src/exercises/25/exercise.js
+++ src/exercises/25/exercise.js
@@ -1,1 +1,12 @@
 // Read what is a closure and give an example here
+
+//A closure is a function having access to the parent scope, even after the parent function has closed.
+/*
+const add = (function () {
+  let counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+add();
+
+*/
