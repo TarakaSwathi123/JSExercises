e41df8cd898f730c00115c3877dbded1e835926f
Tarak <tarakaswathi2001@gmail.com>
2023-01-24 13:22:48 +0530

    24th exercise done

--- src/exercises/24/exercise.js
+++ src/exercises/24/exercise.js
@@ -2,3 +2,6 @@
 // - Share the resource link here.
 // - Update all the exercises you have finished above by following the style guide.
 // From the next exercise write your code following the style guide
+
+
+//resource link :https://developer.mozilla.org/en-US/docs/MDN/Writing_guidelines/Writing_style_guide/Code_style_guide/JavaScript
