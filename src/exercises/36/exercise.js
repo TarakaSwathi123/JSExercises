6148b0a6f84e0d77919a83909e16c33d53560780
Tarak <tarakaswathi2001@gmail.com>
2023-01-25 15:32:31 +0530

    36th exercide done

--- src/exercises/36/exercise.js
+++ src/exercises/36/exercise.js
@@ -21,3 +21,10 @@ console.log(A() ?? C() ?? D());
 console.log(B() ?? D());
 
 //Comment the outputs below and reason
+
+// first A was called is printed because its console.log statement ,then undefined is returned 
+//so right side part of Null coalescing operator is called
+//then print statement: c was called is printed then it returned null so again right side part of operator is called
+//it prints d was called and foo
+
+//when B was called as it doesn't return undefined or null, left side part i.e B is called and printed B was called and false
