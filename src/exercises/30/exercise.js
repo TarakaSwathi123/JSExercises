c8beaa4821b8ca3366fdd828fc5bc857718b363b
Tarak <tarakaswathi2001@gmail.com>
2023-01-25 10:27:00 +0530

    30th exercise done

--- src/exercises/30/exercise.js
+++ src/exercises/30/exercise.js
@@ -3,3 +3,23 @@
 // - Stop the timer at exactly 1min
 // - Display it on a HTML page
 // - What is the function that you have used?
+function timer(){
+    let time=0;
+    var mainTimer=setInterval(incrementer,1000);
+    function incrementer(){
+        time=time+1;
+        //console.log("tick");
+        document.getElementsByTagName("body")[0].innerHTML = `tick ${time}`;
+        if (time==60){
+            clearInterval(mainTimer);
+        }
+       
+    }
+    
+}
+timer();
+
+//console.log(timer());
+
+//window.onload = timer();
+
