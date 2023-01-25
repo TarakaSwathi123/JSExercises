bfc02751e6f063d6b45a22da5c806376471adec9
Tarak <tarakaswathi2001@gmail.com>
2023-01-25 15:47:48 +0530

    26th exercise done

--- src/exercises/26/exercise.js
+++ src/exercises/26/exercise.js
@@ -1,9 +1,13 @@
 // Create a Counter using closures.
 // - counter should have "increment","decrement" and "value" functions.
 // - Is there a anonymus function used here? Is so where is it?
-// - What is an IIFE? Have we used one here?
+//yes,in the first line 
+// - What is an IIFE? Have we used one here?//An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined
+//yes we used increment,decrement,value
 // - Discuss about "Revealing Module Pattern" and how we have used it.
+//Revealing module pattern is a design pattern, which let you organise your javascript code in modules, and gives better code structure.
 // - What are higher order functions?
+//a function which takes another function as an argument or returns a function is known as a higher order function
 
 const counter = (function () {
   //Your code goes here.
